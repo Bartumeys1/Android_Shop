044aef51a460356b558ee4eb804181d3c842884d
Bartumeys1 <andriy_stepaniuk@ukr.net>
2023-05-03 20:23:59 +0300

    Werked Autorization

--- Sim23/Sim23/Program.cs
+++ Sim23/Sim23/Program.cs
@@ -1,7 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.IdentityModel.Tokens;
+using Sim23.Abstract;
 using Sim23.Data;
+using Sim23.Data.Entitys.Identity;
 using Sim23.Mapper;
+using Sim23.Services;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,9 +20,42 @@ builder.Services.AddDbContext<AppEFContext>(opt =>
 });
 
 
+var signinKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetValue<String>("JWTSecretKey")));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(cfg =>
+{
+    cfg.RequireHttpsMetadata = false;
+    cfg.SaveToken = true;
+    cfg.TokenValidationParameters = new TokenValidationParameters()
+    {
+        IssuerSigningKey = signinKey,
+        ValidateAudience = false,
+        ValidateIssuer = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+
+builder.Services.AddIdentity<UserEntity, RoleEntity>(options =>
+{
+    options.Stores.MaxLengthForKeys = 128;
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 5;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+}).AddEntityFrameworkStores<AppEFContext>().AddDefaultTokenProviders();
+
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IJwtTokenService, JwtTokenService>();
 
 
 //Add AutoMapper
@@ -25,6 +65,7 @@ builder.Services.AddAutoMapper(typeof(AppMapProfile));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -34,6 +75,13 @@ var app = builder.Build();
     app.UseSwagger();
     app.UseSwaggerUI();
 //}
+
+app.UseCors(options =>
+{
+    options.AllowAnyMethod()
+    .AllowAnyOrigin()
+    .AllowAnyHeader();
+});
 
 //Відкриває доступ до статичних файлів у папці
 var dir = Path.Combine(Directory.GetCurrentDirectory(),"images");
@@ -48,6 +96,7 @@ app.UseStaticFiles(new StaticFileOptions
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
